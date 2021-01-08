4e64f02f956278df0600bbf35f815e12d91a4278
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-01-07 18:10:42 -0800

    Update Assignment4.js

--- Assignment4.js
+++ Assignment4.js
@@ -3,10 +3,13 @@
  */
 
 // Import the node libraries //
-
-const path = require("path")
+const path = require("path");
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
 
 // Create our express application //
+const app = express();
 
 // Sets it so we can use ejs as our template engine //
 
@@ -22,7 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* ROUTES */
 
 // Handle Get Request to homepage a.k.a "/" //
+app.get("/", (req, res, next) => {
+    res.send("Hello")
+})
 
 // Handle POST Request to create contact a.k.a "/create_contact" //
 
 // Start the app //
+app.listen(3000);
