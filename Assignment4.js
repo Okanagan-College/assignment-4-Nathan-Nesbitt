fab6715e74a42561431c22542a25b628a364ca72
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-23 19:24:45 -0800

    Added in SQL code in node and POST handler

--- Assignment4.js
+++ Assignment4.js
@@ -22,13 +22,37 @@ app.use(express.urlencoded())
 // Set the port //
 const port = 3005;
 
+// Connect to the database //
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "contacts",
+    password: "password",
+    database: "contacts"
+})
+
+connection.connect();
+
 // Handle GET requests to homepage //
 app.get("/", (req, res) => {
-    res.render("Assignment4.ejs");
+    connection.query("SELECT name, email, phone, notes FROM people", (err, result) => {
+        res.render("Assignment4.ejs", {people: result});
+    })
 })
 
 // Create post handler //
+app.post("/create_contact", (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var notes = req.body.notes;
 
+    connection.query(
+        "INSERT INTO people(name, email, phone, notes) VALUES (?, ?, ?, ?)",
+        [name, email, phone, notes]
+    )
+
+    res.redirect("/");
+})
 
 // Starts the application //
 app.listen(port);
