60cf4c58d387a358e778ce8d8e1b9449d439d99c
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-23 18:59:49 -0800

    Add MySQL code

--- Assignment4.js
+++ Assignment4.js
@@ -4,11 +4,17 @@ const express = require("express");
 // Import MySQL //
 const mysql = require("mysql");
 
+// Import path utilities //
+const path = require('path');
+
 // Creating the app for express //
 const app = express()
 
 // Set the view engine //
 app.set("view engine", "ejs");
+
+// Add the public folder to the app //
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Handle body data //
 app.use(express.urlencoded())
@@ -20,6 +26,9 @@ const port = 3005;
 app.get("/", (req, res) => {
     res.render("Assignment4.ejs");
 })
+
+// Create post handler //
+
 
 // Starts the application //
 app.listen(port);
